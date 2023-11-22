6677200941e8d8f17883b40f9b26bcb34739846b
Mehmet Ali Soylu <mhmtsoylu1928@gmail.com>
2023-11-22 20:11:00 +0300

    Implement buttons

--- draw_surface.py
+++ draw_surface.py
@@ -1,43 +1,45 @@
 from tkinter import Canvas
-from settings import *
+from settings import * 
 
 class DrawSurface(Canvas):
-    def __init__(self,parent, color_string, brush_float):
-        super().__init__(parent,background=CANVS_BG,
-                         bd=0,
-                         highlightthickness=0,
-                         relief="ridge")
-        self.pack(expand=True,fill="both")
+	def __init__(self, parent, color_string, brush_float, erase_bool):
+		
+		# setup 
+		super().__init__(master = parent, background = CANVS_BG, bd = 0, highlightthickness = 0, relief = 'ridge')
+		self.pack(expand = True, fill = 'both')
 
-        self.color_string = color_string
-        self.brush_float = brush_float
-        self.allow_draw = False
+		# data 
+		self.color_string = color_string
+		self.brush_float = brush_float
+		self.erase_bool = erase_bool
+		self.allow_draw = False
 
-        # start pos
-        self.old_x = None
-        self.old_y = None
+		# start pos
+		self.old_x = None
+		self.old_y = None
 
-        self.bind("<Motion>", self.draw)
-        self.bind("<Button>", self.activate_draw)
-        self.bind("<ButtonRelease>", self.deactivate_draw)
-    
-    def draw(self,event):
-        if self.allow_draw:
-            if self.old_x and self.old_y:
-                self.create_brush_line((self.old_x, self.old_y),
-                                 (event.x, event.y))
-            self.old_x = event.x
-            self.old_y = event.y
+		# input 
+		self.bind('<Motion>', self.draw)
+		self.bind('<Button>', self.activate_draw)
+		self.bind('<ButtonRelease>', self.deactivate_draw)
 
-    def create_brush_line(self, start, end):
-        brush_size = self.brush_float.get() * 100
-        self.create_line(start, end, fill=f"#{self.color_string.get()}", width=brush_size,capstyle="round")
+	def draw(self, event):
+		if self.allow_draw:
+			if self.old_x and self.old_y: 
+				self.create_brush_line((self.old_x, self.old_y), (event.x, event.y))
+			self.old_x = event.x
+			self.old_y = event.y
 
-    def activate_draw(self,event):
-        self.allow_draw = True
-        self.create_brush_line((event.x,event.y),(event.x+1,event.y+1))
+	def create_brush_line(self, start, end):
+		brush_size = self.brush_float.get() * 10 ** 2
+		color = f'#{self.color_string.get()}' if not self.erase_bool.get() else '#FFF'
+		self.create_line(start, end, fill = color, width = brush_size, capstyle = 'round')
 
-    def deactivate_draw(self,event):
-        self.allow_draw = False
-        self.old_x = None
-        self.old_y = None
+	def activate_draw(self, event):
+		self.allow_draw = True
+		self.create_brush_line((event.x, event.y), (event.x + 1, event.y + 1))
+
+	def deactivate_draw(self, event):
+		self.allow_draw = False
+		self.old_x = None
+		self.old_y = None
--- main.py
+++ main.py
@@ -1,33 +1,38 @@
-import customtkinter as ctk
+import customtkinter as ctk 
 from draw_surface import DrawSurface
 from tool_panel import ToolPanel
 
 class App(ctk.CTk):
-    def __init__(self):
-        super().__init__()
-        self.geometry("800x600")
-        self.title("Paint App")
-        # self.iconbitmap("empty.ico")
-        ctk.set_appearance_mode("light")
+	def __init__(self):
+		super().__init__()
+		self.geometry('800x600')
+		self.title('')
+		self.iconbitmap('empty.ico')
+		ctk.set_appearance_mode('light')
 
-        # data
-        self.color_string = ctk.StringVar(value="000")
-        self.brush_float = ctk.DoubleVar(value=1)
+		# data 
+		self.color_string = ctk.StringVar(value = '000')
+		self.brush_float = ctk.DoubleVar(value = 1)
+		self.erase_bool = ctk.BooleanVar()
 
-        # widgets
-        DrawSurface(self, self.color_string, self.brush_float)
-        ToolPanel(self,self.brush_float,self.color_string)
+		# widgets 
+		self.draw_surface = DrawSurface(self, self.color_string, self.brush_float, self.erase_bool)
+		ToolPanel(self, self.brush_float, self.color_string, self.erase_bool, self.clear_canvas)
+		self.erase_bool.set(False)
 
-        # mousewheel event
-        self.bind("<MouseWheel>",self.adjust_brush_size)
+		# mousewheel event
+		self.bind('<MouseWheel>', self.adjust_brush_size)
 
-        self.mainloop()
+		self.mainloop()
 
-    def adjust_brush_size(self,event):
-        direction = int(event.delta / abs(event.delta))
-        new_brush_size = self.brush_float.get() + 0.05 * direction
-        new_brush_size = max(0.2,min(1, new_brush_size))
-        self.brush_float.set(new_brush_size)
+	def clear_canvas(self):
+		self.draw_surface.delete('all')
 
-if __name__ == "__main__":
-    App()
+	def adjust_brush_size(self, event):
+		direction = int(event.delta / abs(event.delta))
+		new_brush_size = self.brush_float.get() + 0.05 * direction
+		new_brush_size = max(0.2,min(1, new_brush_size))
+		self.brush_float.set(new_brush_size)
+
+if __name__ == '__main__':
+	App()
